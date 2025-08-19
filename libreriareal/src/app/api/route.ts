887746999d38fd2aabcd7978fe68c8fbf5f07da2
Vyncen Lara <Vyncen.lara@gmail.com>
2025-08-18 22:20:27 -0600

    Conexion a base y guardar datos 4/8

--- libreriareal/src/app/api/route.ts
+++ libreriareal/src/app/api/route.ts
@@ -1,27 +1,35 @@
 import { NextResponse, NextRequest } from "next/server";
+import postgres from "postgres";
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
   const { title, description, author } = data || {};
 
   if (!title || !description || !author) {
-    return NextResponse.json({ error: "Missing fields" });
+    return NextResponse.json({ error: "Missing fields" }, { status: 422 });
   }
 
   if (typeof title !== "string" || title.length < 3) {
-    return NextResponse.json({ error: "Invalid title" });
+    return NextResponse.json({ error: "Invalid title" }, { status: 422 });
   }
 
   if (typeof description !== "string" || description.length < 10) {
-    return NextResponse.json({ error: "Invalid description" });
+    return NextResponse.json({ error: "Invalid description" }, { status: 422 });
   }
 
   if (typeof author !== "string" || author.length < 3) {
-    return NextResponse.json({ error: "Invalid author" });
+    return NextResponse.json({ error: "Invalid author" }, { status: 422 });
   }
 
+  const sql = postgres("postgresql://postgres.iqgvhyftksnepwlvxfsz:Escav10Lara@@aws-0-us-east-1.pooler.supabase.com:6543/postgres");
+  await sql`
+    INSERT INTO "Posts" (title, description, author)
+    VALUES (${title}, ${description}, ${author})
+  `
+  ;
+
   return NextResponse.json({
-    message: "Data validated strictly",
+    message: "Post saved successfully",
     data: { title, description, author }
   });
 }
